2a8f651c0b7e2007478bb1dd5fed8bf67d66d387
Mariam Ahmed <87809525+MariamElKawakji@users.noreply.github.com>
2023-10-27 00:06:34 +0200

    Add files via upload

---
+++ SEG_Registers.h
@@ -0,0 +1,13 @@
+/*
+ * SEG_Registers.h
+ *
+ *  Created on: Oct 27, 2023
+ *      Author: Marii
+ */
+
+#ifndef SEVEN_SEGMENT_SEG_REGISTERS_H_
+#define SEVEN_SEGMENT_SEG_REGISTERS_H_
+
+
+
+#endif /* SEVEN_SEGMENT_SEG_REGISTERS_H_ */
