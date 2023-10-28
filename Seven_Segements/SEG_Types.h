f55883293b9e14569ed96e2580791427f86f7c40
Mohamedsaadeldin71 <mohamedsaadeldin45@gmail.com>
2023-10-28 22:55:09 +0200

    Timer0

---
+++ Seven_Segements/SEG_Types.h
@@ -0,0 +1,13 @@
+/*
+ * SEG_Types.h
+ *
+ *  Created on: Oct 27, 2023
+ *      Author: Marii
+ */
+
+#ifndef SEVEN_SEGMENT_SEG_TYPES_H_
+#define SEVEN_SEGMENT_SEG_TYPES_H_
+
+
+
+#endif /* SEVEN_SEGMENT_SEG_TYPES_H_ */
