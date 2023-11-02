6f1947f41c25f9449ae4b64dea87695712cf2e06
fatmaeltahan <fatmaelthan8@gmail.com>
2023-11-02 21:53:33 +0200

    ...

---
+++ Solenoid driver/Solenoid_driver/APP/app.h
@@ -0,0 +1,26 @@
+/*
+ * app.h
+
+
+ *
+ *  Created on: Oct 26, 2023
+ *      Author: dell
+ */
+
+
+
+
+
+
+#ifndef APP_APP_H_
+#define APP_APP_H_
+
+#include "../HAL/Solenoid/Solenoid_interface.h"
+
+
+
+void APP_app_init();
+void APP_app_start();
+
+
+#endif /* APP_APP_H_ */
---
+++ Solenoid driver/Solenoid_driver/Debug/MCAL/DIO/DIO.d
@@ -0,0 +1,19 @@
+MCAL/DIO/DIO.o MCAL/DIO/DIO.o: ../MCAL/DIO/DIO.c \
+  ../MCAL/DIO/DIO_interface.h ../MCAL/DIO/common_macros.h \
+  ../MCAL/DIO/DIO_registers.h ../MCAL/DIO/DIO_Types.h \
+  ../MCAL/DIO/DIO_private.h ../MCAL/DIO/DIO_config.h \
+  ../MCAL/DIO/DIO.stdtypes.h
+
+../MCAL/DIO/DIO_interface.h:
+
+../MCAL/DIO/common_macros.h:
+
+../MCAL/DIO/DIO_registers.h:
+
+../MCAL/DIO/DIO_Types.h:
+
+../MCAL/DIO/DIO_private.h:
+
+../MCAL/DIO/DIO_config.h:
+
+../MCAL/DIO/DIO.stdtypes.h:
