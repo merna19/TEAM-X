b0dc690ea648246ec4b6d5b0112e28d2cb3eacc0
merna19 <90727508+merna19@users.noreply.github.com>
2023-11-03 22:52:48 +0200

    Update ultrasonic.c
    exchanged magic  numbers with macros

--- ULTRASONIC/ULTRASONIC/ultrasonic.c
+++ ULTRASONIC/ULTRASONIC/ultrasonic.c
@@ -13,30 +13,31 @@ void HAL_ULTRASONIC_EInit(void)
 	trig->pin_num=TRIGGER_PIN;
 	trig->port_num=TRIGGER_PORT;
 	trig->Direction=0xFF;*///set it as output
-	DIO_vsetbitdir(TRIGGER_PORT,TRIGGER_PIN,1);
+	DIO_vsetbitdir(TRIGGER_PORT,TRIGGER_PIN,HIGH);
 }
 
 void HAL_ULTRASONIC_u8ReadDistance(unsigned short* distance)
 {	
 	unsigned short a,high,b;
-	TCCR1A = 0;
+	TCCR1A = LOW;
 	TIFR = (1<<ICF1);  	/* Clear ICF (Input Capture flag)  */
-	DIO_vpinwrite(TRIGGER_PORT,TRIGGER_PIN,1);
+	DIO_vpinwrite(TRIGGER_PORT,TRIGGER_PIN,HIGH);
 	_delay_us(50);
-	DIO_vpinwrite(TRIGGER_PORT,TRIGGER_PIN,0);
+	DIO_vpinwrite(TRIGGER_PORT,TRIGGER_PIN,LOW);
 	
-	TCCR1B = 0xc1;  	/* Rising edge, no prescaler , noise canceler*/
-	while ((TIFR&(1<<ICF1)) == 0);
+	TCCR1B = RISE_NOPRESCALAR;  	/* Rising edge, no prescaler , noise canceler*/
+	while ((TIFR&(1<<ICF1)) == LOW);
 	a = ICR1;  		/* Take value of capture register */
 	TIFR = (1<<ICF1);  	/* Clear ICF flag */
-	TCCR1B = 0x81;  	/* Falling edge, no prescaler ,noise canceler*/
-	while ((TIFR&(1<<ICF1)) == 0);
+	TCCR1B = FALL_NOPRESCALAR;  	/* Falling edge, no prescaler ,noise canceler*/
+	while ((TIFR&(1<<ICF1)) == LOW);
 	b = ICR1;  		/* Take value of capture register */
 	TIFR = (1<<ICF1);  	/* Clear ICF flag */
-	TCNT1=0;
-	TCCR1B = 0;  		/* Stop the timer */
+	TCNT1=LOW;
+	TCCR1B = LOW;  		/* Stop the timer */
 	high=b-a;
-	*distance=((high*34600)/(F_CPU*2));
+	*distance=((high*SPEED)/(F_CPU*2));
 }
+
 
 
