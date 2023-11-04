0202ba7fa3a840a85ea69c1af69426fa8523b670
Mohamedsaadeldin71 <mohamedsaadeldin45@gmail.com>
2023-11-04 10:28:28 +0200

    Updated Timer0

--- Timer0/main.c
+++ Timer0/main.c
@@ -11,18 +11,19 @@
 #include <util/delay.h>
 
 int counter = 0;
-/*
+
 ISR(TIMER0_OVF_vect)
 {
 	counter ++;
-	if(counter == 10)
+
+	if(counter == 4)
 	{
 		PORTA = PORTA ^ (1<<PINA7);
 		counter = 0;
 	}
 
 }
-*/
+
 /*
 ISR (TIMER0_COMP_vect)
 {
@@ -39,17 +40,14 @@ int main(void)
 {
 	DDRA = 0xFF;
 	PORTA =0x00;
-	//SREG |=(1<<7);
+	SREG |=(1<<7);
+	MCAL_Timer0_Init(Prescaler_64, 0, 0);
 
 	//MCAL_Timer0_OVF_Init(3);
 	//MCAL_Timer0_CTC_Init(2,250);
 
 	while(1)
 	{
-		MCAL_Timer0_PCPWM_Init(1,90);
-		_delay_ms(1000);
-		MCAL_Timer0_PCPWM_Init(1,125);
-		_delay_ms(1000);
 
 
 	}
