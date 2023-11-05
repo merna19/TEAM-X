88663f19854d4d76b7da987ebc9c4d6c84cf86fc
merna19 <90727508+merna19@users.noreply.github.com>
2023-11-06 01:19:17 +0200

    ULTRASONIC SENSOR COMMIT

---
+++ ultrasonic sensor/ultrasonic sensor/ultrasonic sensor.c
@@ -0,0 +1,47 @@
+#include <avr/io.h>
+#include "LCD.h"
+#define  F_CPU 1000000UL
+#include <util/delay.h>
+int main(void)
+{
+	LCD_vInit();
+	unsigned short a,b,high,distance;
+	DIO_vsetPINDir('D',7,1);
+    while(1)
+    {	
+		TCCR1A = 0;
+		TIFR = (1<<ICF1);  	/* Clear ICF (Input Capture flag)  */
+		DIO_write('D',7,1);
+		_delay_us(50);
+		DIO_write('D',7,0);
+	
+	    TCCR1B = 0xc1;  	/* Rising edge, no prescaler , noise canceler*/
+	    while ((TIFR&(1<<ICF1)) == 0);
+	    a = ICR1;  		/* Take value of capture register */
+	    TIFR = (1<<ICF1);  	/* Clear ICF flag */
+	    TCCR1B = 0x81;  	/* Falling edge, no prescaler ,noise canceler*/
+	    while ((TIFR&(1<<ICF1)) == 0);
+	    b = ICR1;  		/* Take value of capture register */
+	    TIFR = (1<<ICF1);  	/* Clear ICF flag */
+		TCNT1=0;
+	    TCCR1B = 0;  		/* Stop the timer */
+		high=b-a;
+		distance=((high*34600)/(F_CPU*2)) ;
+		if (distance>=80)
+		{
+			LCD_clearscreen();
+			LCD_vSend_string("no object");
+			_delay_ms(500);
+		}
+		else
+		{
+		LCD_clearscreen();
+		LCD_vSend_string("distance=");
+		LCD_vSend_char((distance/10)+48);
+		LCD_vSend_char((distance%10)+48);
+	    LCD_vSend_string("cm");
+		_delay_ms(500);
+		}		
+	}
+}
+
