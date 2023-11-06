c2dacf1fe03cc7cb83f54b87ce85c19f8dbdc78d
merna19 <90727508+merna19@users.noreply.github.com>
2023-11-06 14:56:27 +0200

    garage service layer

---
+++ garage/garage/Debug/main.d
@@ -0,0 +1,14 @@
+main.d main.o: .././main.c .././GARAGE.h .././Ultrasonic_Interface.h \
+ .././Ultrasonic_Config.h .././LCD.h .././DIO.h .././LCD_Config.h
+
+.././GARAGE.h:
+
+.././Ultrasonic_Interface.h:
+
+.././Ultrasonic_Config.h:
+
+.././LCD.h:
+
+.././DIO.h:
+
+.././LCD_Config.h:
---
+++ garage/garage/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+DIO.c
+
+GARAGE.c
+
+LCD.c
+
+main.c
+
+Ultrasonic_Prog.c
+
