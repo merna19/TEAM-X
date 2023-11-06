e1d9284eed90459466130a023d190092c45927bb
Aya <ayaali969@gmail.com>
2023-11-06 14:07:21 +0200

    Added IR sensor driver

---
+++ HAL/IR_Driver/IR_Private.h
@@ -0,0 +1,13 @@
+/*
+ * IR_Private.h
+ *
+ *  Created on: 5 Nov 2023
+ *      Author: Ayaya
+ */
+
+#ifndef IR_PRIVATE_H_
+#define IR_PRIVATE_H_
+
+
+
+#endif /* IR_PRIVATE_H_ */
---
+++ HAL/IR_Driver/IR_Registers.h
@@ -0,0 +1,13 @@
+/*
+ * IR_Registers.h
+ *
+ *  Created on: 5 Nov 2023
+ *      Author: Ayaya
+ */
+
+#ifndef IR_REGISTERS_H_
+#define IR_REGISTERS_H_
+
+
+
+#endif /* IR_REGISTERS_H_ */
