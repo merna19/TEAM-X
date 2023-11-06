5d988cb6023e24e5f0a7aa546e7c23fab9b61ba3
fatmaeltahan <fatmaelthan8@gmail.com>
2023-11-06 12:12:06 +0200

    .......

---
+++ HAL/Solenoid/Solenoid_config.h
@@ -0,0 +1,17 @@
+/*
+ * Solenoid_config.h
+ *
+ *  Created on: Nov 2, 2023
+ *      Author: dell
+ */
+
+#ifndef HAL_SOLENOID_SOLENOID_CONFIG_H_
+#define HAL_SOLENOID_SOLENOID_CONFIG_H_
+
+#include "../../MCAL/DIO/DIO.stdtypes.h"
+#include  "../../MCAL/DIO/DIO_Types.h"
+
+#define SOLENOID_PORT  PORTD_ID
+#define SOLENOID_PIN  PIN2_ID
+
+#endif /* HAL_SOLENOID_SOLENOID_CONFIG_H_ */
