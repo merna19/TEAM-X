b2fbc9c5abce7f6476c4419fdc7e4f6f913c8571
fatmaeltahan <fatmaelthan8@gmail.com>
2023-11-06 12:10:37 +0200

    .......

---
+++ HAL/PIR/PIR_config.h
@@ -0,0 +1,19 @@
+/*
+ * PIR_config.h
+
+ *
+ *  Created on: Nov 5, 2023
+ *      Author: dell
+ */
+
+#ifndef HAL_PIR_PIR_CONFIG_H_
+#define HAL_PIR_PIR_CONFIG_H_
+
+/* PIR output  port */
+#define PIR_port  PORTD_ID
+/* PIR output pin */
+#define PIR_pin  PIN2_ID
+
+
+
+#endif /* HAL_PIR_PIR_CONFIG_H_ */
